32ceb3828c9a3ef500b53ca82e331917ef24a623
SuGyoung Yun <162414246+sinnarasam@users.noreply.github.com>
2026-01-12 11:21:07 +0000

    공개 Google Sheet을 읽어오는 기능 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,13 @@ st.write("입력된 의견:", feedback)
 
 # 이미지 출력
 st.image("https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/4utc/image/xrVTKtEGE1sWCmwTWWDPoDJaPnY.jpg", caption="AI WORLD!", use_container_width=True)
+
+import streamlit as st
+import pandas as pd
+
+st.title("1️⃣ ✅ 공개 Google Sheet 읽기")
+st.info("📘 누구나 볼 수 있도록 공개된 시트를 Pandas로 직접 불러오는 가장 간단한 방법입니다.\n📎 링크는 반드시 `export?format=csv` 형태로 설정하세요.")
+
+csv_url1 = "https://docs.google.com/spreadsheets/d/1VC_q8HJfIufjGVR2zGRcJjBgkefIbp6Pv01rQ1uvoXI/export?format=csv"
+df1 = pd.read_csv(csv_url1)
+st.dataframe(df1)
